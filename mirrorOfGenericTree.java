aeac18a72282e5d235ce4b8dd13111ad9c07380e
Abhay <chauhan27.abhay@gmail.com>
2020-08-07 12:27:35 +0530

    mirror of Gneric Tree

---
+++ mirrorOfGenericTree.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.Collections;
+class mirrorOfGenericTree{
+
+    private static class Node{
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
+    public static Node constructor(int[] arr){
+        Node root = new Node();
+        Stack<Node> st = new Stack<>();
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == -1){
+                st.pop();
+            }else{
+                Node temp = new Node();
+                temp.data= arr[i];
+                if(st.size()>0){
+                    st.peek().children.add(temp);
+                }else{
+                    root = temp;
+                }
+                st.push(temp);
+            }
+        } return root;
+    }
+
+    public static void mirrorOfGenericTree(Node node){
+        for(Node child: node.children){
+            mirrorOfGenericTree(child);
+        }
+        Collections.reverse(node.children);
+    }
+
+    public static void display(Node node){
+        String str = node.data + " -> ";
+
+        for(Node child: node.children){
+            str += child.data + ", ";
+        }
+        str += ".";
+        System.out.println(str);
+        
+        for(Node child: node.children){
+            display(child);
+        }
+    }
+
+    public static void main(String[] args){
+        int [] arr = {10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
+
+        Node root = constructor(arr);
+        mirrorOfGenericTree(root);
+        display(root);
+
+    }
+}
